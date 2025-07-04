e9a978e27cb76db7cfe54991b98877368a0cc133
jonathandiouf82 <jonathan.diouf@epitech.eu>
2025-07-04 16:12:16 +0200

    {feat} | init

---
+++ src/components/header/header.js
@@ -0,0 +1,15 @@
+export function Header() {
+    return (
+        <header>
+            <div class="logo">MonLogo</div>
+            <nav>
+                <ul class="nav-links">
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">À propos</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
